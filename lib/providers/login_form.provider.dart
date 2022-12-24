9434a92bdc9f316b9ab08d9be9bcbf5a7a88d722
Miguel Martin <miguel.martin@siptize.com>
2022-12-24 11:17:40 +0100

    added firebase

--- lib/providers/login_form.provider.dart
+++ lib/providers/login_form.provider.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginFormProvider extends ChangeNotifier {
@@ -5,15 +6,33 @@ class LoginFormProvider extends ChangeNotifier {
   String email = '';
   bool isLoading = false;
 
-  bool isValidForm() {
-    print(formKey.currentState?.validate());
-    return formKey.currentState?.validate() ?? false;
-  }
+  bool isValidForm() => formKey.currentState?.validate() ?? false;
 
   void sendLink() async {
     isLoading = true;
-    await Future.delayed(Duration(seconds: 3));
+    notifyListeners();
+    // // await Future.delayed(Duration(seconds: 1));
+
+    var acs = ActionCodeSettings(
+        // URL you want to redirect back to. The domain (www.example.com) for this
+        // URL must be whitelisted in the Firebase Console.
+        url: 'https://www.example.com/finishSignUp?cartId=1234',
+        // This must be true
+        handleCodeInApp: true,
+        // iOSBundleId: 'com.example.ios',
+        androidPackageName: 'miguel.martin.taskii',
+        // installIfNotAvailable
+        androidInstallApp: true,
+        // minimumVersion
+        androidMinimumVersion: '12');
+
+    FirebaseAuth.instance
+        .sendSignInLinkToEmail(email: email, actionCodeSettings: acs)
+        .catchError(
+            (onError) => print('Error sending email verification $onError'))
+        .then((value) => print('Successfully sent email verification'));
 
     isLoading = false;
+    notifyListeners();
   }
 }
